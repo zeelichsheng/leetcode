bdd95aa92e730e06e1c12ad90b797bd5ed979c6d
Yu Sheng <zeelichsheng@hotmail.com>
2016-03-10 22:16:14 -0800

    Model: add TreeLinkNode model

---
+++ src/leetcode/model/TreeLinkNode.java
@@ -0,0 +1,12 @@
+package leetcode.model;
+
+public class TreeLinkNode {
+  public int val;
+  public TreeLinkNode left;
+  public TreeLinkNode right;
+  public TreeLinkNode next;
+  
+  public TreeLinkNode(int val) {
+    this.val = val;
+  }
+}
