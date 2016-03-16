0f7e829e3f43edd7bd7d6c06b8bbc62ad52dd0d9
Yu Sheng <zeelichsheng@hotmail.com>
2016-03-16 16:27:26 -0700

    Model: add RandomListNode model

---
+++ src/leetcode/model/RandomListNode.java
@@ -0,0 +1,11 @@
+package leetcode.model;
+
+public class RandomListNode {
+  public int label;
+  public RandomListNode next;
+  public RandomListNode random;
+
+  public RandomListNode(int label) {
+    this.label = label;
+  }
+}
